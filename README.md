fb4d25becd1f75e3beed7a77880b677444a7c7d2
Joslyn Rosbrook <joslynrosbrook@Joslyns-MacBook-Air.local>
2014-01-16 15:57:23 -0800

    zurb site and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Use of Zurb Foundation to Build and Customize Store Site
+
+Based off of Railscast #417
+
+Ruby, Rails 4, Foundation Rails 4
+
+![ScreenShot](http://i.imgur.com/QR6ByNc.png)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoreZurb::Application.config.secret_key_base = 'd4caaca6e4bbd7eb147f9b52f3814afe94f02c74f26307d1ff7e5b3fb456f90d1fb61aac8c2108cadaeb9340434a29baadef7243c31973719623bb078d133899'
